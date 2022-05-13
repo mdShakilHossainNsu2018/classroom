100b481a26460cfadebff5682af533868528a4fe
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2022-05-13 09:36:39 +0600

    docker prod added

--- web_frontend/src/App.vue
+++ web_frontend/src/App.vue
@@ -148,21 +148,21 @@
         },
 
         created: function(){
-            this.socket = new WebSocket('ws://localhost:8000/ws/siteblocker/');
-            this.socket.onopen = function(event){
-                console.log("ws connected...")
+            // this.socket = new WebSocket('ws://localhost:8000/ws/siteblocker/');
+            // this.socket.onopen = function(event){
+            //     console.log("ws connected...")
 
-                console.log(event)
-            }
+            //     console.log(event)
+            // }
 
-            this.socket.onmessage = function(e) {
-                const data = JSON.parse(e.data);
-                console.log(data)
-            }
+            // this.socket.onmessage = function(e) {
+            //     const data = JSON.parse(e.data);
+            //     console.log(data)
+            // }
 
-            this.socket.onclose = function() {
-                console.error('Chat socket closed unexpectedly');
-            }
+            // this.socket.onclose = function() {
+            //     console.error('Chat socket closed unexpectedly');
+            // }
         },
 
         methods: {
